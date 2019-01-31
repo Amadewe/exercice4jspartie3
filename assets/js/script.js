5216386cf88992af272fb2e4dbdf1b57ddfa0306
Amadewe <dewevre.amandine@gmail.com>
2019-01-31 12:12:32 +0100

    Correction

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,17 @@
-// mettre les bordures des inputs en rouge si les mots de passe sont différents. Sinon les mettre en vert.
-// je récupères les deux valeurs de mes éléments avec .value
-function  passewordmatch() {
-var password = document.getElementById('password').value;
-var confirmPassword = document.getElementById('confirmPassword').value;
-// On crée une bouche if si la condition est bonne alors la propriété style.border exécute la valeur : vert
-if (password === confirmPassword){
-document.getElementById('password').style.border= 'solid green';
-document.getElementById('confirmPassword').style.border= 'solid green';
-// si elle ne correspond pas alors la propriété style.border exécute la valeur : rouge
-}else {
-document.getElementById('password').style.border= 'solid red';
-document.getElementById('confirmPassword').style.border= 'solid red';
-}
+//fonction pour valider que les mots de passes sont identiques.
+function passewordmatch()
+{
+  //On stocke tout l'élement dans la variable pour utiliser la value et le borderColor sans réutiliser document.getElementById.
+    var password = document.getElementById('password');
+  var confirmPassword = document.getElementById('confirmPassword');
+  //!= ça veut dire 'différent de'
+  //si les deux sont différent à la validation on vert que les bordures soit rouge.
+  if (password.value != confirmPassword.value) {
+    confirmPassword.style.borderColor='red';
+    password.style.borderColor='red';
+    // ou alors si les deux sont égaux à la validation bordure green.
+  }else{
+    confirmPassword.style.borderColor='green';
+    password.style.borderColor='green';
+  }
 }
